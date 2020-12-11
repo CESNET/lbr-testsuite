2461fd1661f3905e3acf3c416b0f41eb7af0f4ba
Jan Kucera <jan.kucera@cesnet.cz>
2020-12-12 00:18:12 +0100

    spirentlib: introduce Spirent REST API wrapper
    This commit adds a new Spirent API class compatible with our previous
    classes, e.g. StcPythonTCP, etc. The REST API benefits from the fact
    that it is oficially supported and there is also its official client
    which runs at the windows server side (called stcweb).
    
    For more details, have a look at:
    https://github.com/Spirent/py-stcrestclient/tree/master

---
+++ modules/spirent/spirentlib/stcapi/StcPythonREST.py
@@ -0,0 +1,29 @@
+"""
+Author(s): Jan Kucera <jan.kucera@cesnet.cz>
+
+Copyright: (C) 2020 CESNET, z.s.p.o.
+
+Spirent REST API wrapper class.
+"""
+
+import os
+
+try:
+    from stcrestclient.stcpythonrest import StcPythonRest as StcPython
+except ImportError:
+    raise ImportError(
+        "Unable to import 'stcrestclient'. Install latest using 'pip install -U stcrestclient'."
+    )
+
+class StcPythonREST(StcPython):
+    """Spirent REST API wrapper class to simulate the same interface as
+    our previous Spirent API classes, e.g. StcPythonTCP, etc. It is based
+    on the official Spirent REST API client for python. For details have
+    a look at https://github.com/Spirent/py-stcrestclient/tree/master.
+    """
+
+    def __init__(self, host, port):
+        os.environ['STC_REST_API'] = str(1)
+        os.environ['STC_SERVER_ADDRESS'] = str(host)
+        os.environ['STC_SERVER_PORT'] = str(port)
+        super().__init__()
