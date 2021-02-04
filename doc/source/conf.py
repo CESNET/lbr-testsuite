583198c1500e971cba25ae253a2840b5d72f2818
Tran Dominik <tran@cesnet.cz>
2021-02-04 20:04:35 +0100

    docs: add custom CSS to enforce one parameter per line

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -60,4 +60,8 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['css']
+
+html_css_files = [
+    'one_parameter_per_line.css',
+]
