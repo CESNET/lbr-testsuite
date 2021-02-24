13b3394dcb593f37a48cbeba0fd6e4785c2810fe
Tran Dominik <tran@cesnet.cz>
2021-02-24 14:29:45 +0100

    lbr_testsuite: include spirent and spirentlib in __init__ file

--- lbr_testsuite/__init__.py
+++ lbr_testsuite/__init__.py
@@ -2,5 +2,14 @@ from .ipconfigurer import ipconfigurer
 from .trex_tools import trex_instances
 from .trex_tools import trex_stl_stream_generator
 from .trex_tools import trex_astf_profile_generator
+from .spirent import spirent
+from .spirent.spirentlib import spirentlib
 
-__all__ = ['ipconfigurer', 'trex_instances', 'trex_stl_stream_generator', 'trex_astf_profile_generator']
+__all__ = [
+    'ipconfigurer',
+    'trex_instances',
+    'trex_stl_stream_generator',
+    'trex_astf_profile_generator',
+    'spirent',
+    'spirentlib'
+]
