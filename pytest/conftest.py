8576ba7ee83c234c193b06f0cf76d7bc66385fc4
Jakub Suran <xsuran07@stud.fit.vutbr.cz>
2021-09-29 06:29:16 +0000

    lbr_testsuite: move require_root fixture into lbr_testsuite pytest plugin

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -7,19 +7,6 @@ Common pytest fixtures and hooks for testsuite tests.
 """
 
 import logging
-import os
-import pytest
-from pytest_cases import fixture
-
-
-@fixture(scope='session')
-def require_root():
-    """Fixture checking whether a test is running under the root."""
-
-    euid = os.geteuid()
-
-    if euid != 0:
-        raise pytest.skip(f'insufficient permissions, euid: {euid}')
 
 
 def pytest_configure(config):
