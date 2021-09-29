87806fd9fd1840b86f05cc8c37630bf8e83459e1
Jakub Suran <xsuran07@stud.fit.vutbr.cz>
2021-09-29 06:21:46 +0000

    lbr_testsuite: introduce lbr_testsuite pytest plugin

---
+++ lbr_testsuite/pytest/plugin.py
@@ -0,0 +1,7 @@
+"""
+Author(s): Jakub Šuráň <xsuran07@fit.vutbr.cz>
+
+Copyright: (C) 2021 CESNET, z.s.p.o.
+
+Pytest plugin with common fixtures.
+"""
--- setup.py
+++ setup.py
@@ -24,11 +24,18 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
+        "Framework :: Pytest"
     ],
     install_requires=[
         'pyroute2>=0.6.2,<1.',
         'lbr_trex_client',
         'stcrestclient',
+        'pytest'
     ],
     python_requires='>=3.6',
+    entry_points={
+        "pytest11": [
+            "lbr_testsuite = lbr_testsuite.pytest.plugin",
+        ],
+    },
 )
