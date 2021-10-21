03fd1a2fd18d5fe1be6b7edb00881539036063e2
Pavel Krobot <pavel.krobot@cesnet.cz>
2021-10-21 08:38:54 +0200

    API: change API of lbr_testsuite package
    This change unifies approach to lbr_testsuite modules importing,
    simplifying imports from the package. Common functions and
    classes are now also accessible under lbr_testsuite namespace directly.

--- lbr_testsuite/__init__.py
+++ lbr_testsuite/__init__.py
@@ -1,19 +1,5 @@
-from .ipconfigurer import ipconfigurer
-from .executable import executable
-from .trex_tools import trex_instances
-from .trex_tools import trex_stl_stream_generator
-from .trex_tools import trex_astf_profile_generator
-from .spirent import spirent
-from .spirent.spirentlib import spirentlib
-from .common import common
-
-__all__ = [
-    'ipconfigurer',
-    'executable',
-    'trex_instances',
-    'trex_stl_stream_generator',
-    'trex_astf_profile_generator',
-    'spirent',
-    'spirentlib',
-    'common',
-]
+from .common import *  # noqa
+from . import executable  # noqa
+from . import ipconfigurer  # noqa
+from . import spirent  # noqa
+from . import trex_tools  # noqa
