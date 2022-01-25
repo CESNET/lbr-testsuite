4c49d482beb58e49f882ff75c7d3b4da2c86a999
Pavel Krobot <pavel.krobot@cesnet.cz>
2022-01-25 13:19:18 +0000

    pytest: introduce simple sample test for topologies

---
+++ pytest/tests/topology/test_topology.py
@@ -0,0 +1,25 @@
+"""
+Author(s): Pavel Krobot <Pavel.Krobot@cesnet.cz>
+
+Copyright: (C) 2020-2021 CESNET, z.s.p.o.
+
+Demonstration of topology usage. To use test topologies run tests with
+arguments: `--wired-loopback=<ifc>,<pci-address>  --vdevs`. For more
+details on these topology arguments see tests help.
+"""
+
+from lbr_testsuite.topology.topology import select_topologies
+
+
+select_topologies(['wired_loopback', 'vdev_loopback'])
+
+def test_topology(device, generator):
+    print()
+    print('Device: \n'
+        f'    type: {type(device)}\n'
+        f'    name: {device.get_dpdk_name()}\n'
+        f'    name: {device.get_dpdk_args()}\n'
+    )
+    print('Generator: \n'
+        f'    type: {type(generator)}\n'
+    )
