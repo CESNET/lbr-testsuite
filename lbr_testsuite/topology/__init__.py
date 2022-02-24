26d8f1369dd3a7bfa76a46594dc8c5bf620716d1
Pavel Krobot <pavel.krobot@cesnet.cz>
2022-02-24 20:06:27 +0100

    topology: introduce new topology registration mechanism
    Until now, a developer has to implement pytest_generate_tests and
    pytest_collection_modifyitems pytest hooks in a predefined way
    manually for all custom topologies. Using new registration mechanism,
    all registered topologies are kept in a private global dictionary
    within the topology.registration module. A developer can now register
    custom topology using topology_register() function from
    topology.registration module.

--- lbr_testsuite/topology/__init__.py
+++ lbr_testsuite/topology/__init__.py
@@ -2,6 +2,7 @@ from .generator import Generator, NetdevGenerator
 from .device import Device, PciDevice, VdevDevice, RingDevice, PcapLiveDevice
 from .pci_address import PciAddress
 from .topology import Topology, select_topologies
+from .registration import topology_register, registered_topologies
 
 __all__ = [
     'Generator',
@@ -14,4 +15,6 @@ __all__ = [
     'PciAddress',
     'Topology',
     'select_topologies',
+    'topology_register',
+    'registered_topologies',
 ]
