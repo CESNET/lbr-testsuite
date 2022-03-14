b1135fe5e95e7c5b47a0eb7684d294347f3bba64
Pavel Krobot <pavel.krobot@cesnet.cz>
2022-03-14 12:21:23 +0000

    topology: simplify registration
    Remove argument "name" from topology.registration.topology_register()
    function to simplify registration of new topologies. This argument
    has no practical usage and its purpose is only to provide unique
    identifier. To ensure uniqueness, current "option_name" is sufficient.

--- lbr_testsuite/topology/registration.py
+++ lbr_testsuite/topology/registration.py
@@ -10,7 +10,7 @@ Registration of implemented topologies.
 _REGISTERED_TOPOLOGIES = dict()
 
 
-def topology_register(name, option_name):
+def topology_register(option_name):
     """Register a topology implementation in topology module.
 
     Registration ensures creation of a fixture named
@@ -19,17 +19,15 @@ def topology_register(name, option_name):
 
     Parameters
     ----------
-    name: str
-        Unique topology identification.
     option_name: str
         Name of topology option.
     """
 
     global _REGISTERED_TOPOLOGIES
-    assert name not in _REGISTERED_TOPOLOGIES, 'Topology already implemented.'
+    assert option_name not in _REGISTERED_TOPOLOGIES, 'Topology already implemented.'
 
     pseudofixture_name = f'option_{option_name}'
-    _REGISTERED_TOPOLOGIES[name] = dict(
+    _REGISTERED_TOPOLOGIES[option_name] = dict(
         option_name=option_name,
         pseudofixture=pseudofixture_name
     )
