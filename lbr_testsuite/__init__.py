cac3f113ab9b61c53bf2159683cb82452d2aa9f9
Dominik Tran <tran@cesnet.cz>
2023-07-17 12:45:45 +0200

    trex_tools: remove trex_tools
    This code is old and it is not used anymore.
    It will be replaced by newer TRex code in the future.

--- lbr_testsuite/__init__.py
+++ lbr_testsuite/__init__.py
@@ -2,5 +2,4 @@ from . import executable  # noqa
 from . import ipconfigurer  # noqa
 from . import spirent  # noqa
 from . import topology  # noqa
-from . import trex_tools  # noqa
 from .common import *  # noqa
