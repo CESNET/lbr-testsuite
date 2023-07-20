14b4ed9d95e6976fd2763dcb911a151064b3d973
Dominik Tran <tran@cesnet.cz>
2023-07-21 00:00:20 +0200

    topology: introduce trex topology
    TRex topology includes mainly two options:
      --trex-generator
      --wired-trex
    
    Option trex-generator contains <TRex hostname, TRex PCI address>
    and represents single TRex generator on given hostname using
    DPDK-compatible network card given by PCI address.
    
    Option wired-trex contains <list of hostnames, device PCI address>
    and represents hostnames to use (not all --trex-generator may be used)
    and PCI address of network device which processes traffic generated
    by TRex.

--- lbr_testsuite/_pytest_plugins/topology/__init__.py
+++ lbr_testsuite/_pytest_plugins/topology/__init__.py
@@ -1,6 +1,7 @@
-from . import _spirent, _virtual_devices, _wired_loopback
+from . import _spirent, _trex, _virtual_devices, _wired_loopback
 
 
 _wired_loopback._init()
 _virtual_devices._init()
 _spirent._init()
+_trex._init()
