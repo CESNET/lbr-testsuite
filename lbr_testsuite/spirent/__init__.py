db635accaf8777d866a5e8ca57474508c3d09cdc
Kamil Vojanec <vojanec@cesnet.cz>
2023-07-21 09:41:47 +0200

    stream_block: introduce module
    The introduced module is responsible for managing spirent's stream
    blocks. Each stream block can be configured using a provided Spirent
    object. Configuration can be performed by setting the requested packet
    length, bit rate, access VLAN and MAC addresses.

--- lbr_testsuite/spirent/__init__.py
+++ lbr_testsuite/spirent/__init__.py
@@ -1,10 +1,13 @@
 from .spirent import Spirent
 from .spirentlib.spirentlib import STC_API_OFFICIAL, STC_API_PROPRIETARY, StcHandler
+from .stream_block import AbstractStreamBlock, StreamBlock
 
 
 __all__ = [
+    "AbstractStreamBlock",
     "STC_API_PROPRIETARY",
     "STC_API_OFFICIAL",
     "StcHandler",
     "Spirent",
+    "StreamBlock",
 ]
