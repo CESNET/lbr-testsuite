a63fcc63beeb4097957e8a818fbf0fa7bf6a55c4
Kamil Vojanec <vojanec@cesnet.cz>
2023-08-18 13:56:57 +0200

    data_table: introduce extracted module
    The extracted module is used to store measurement data (typically
    throughputs) and save them as CSV files and plot them into PNGs.
    
    The data_table module is independent and was extracted from the DDoS Protector's 'ddp'
    repository.

---
+++ lbr_testsuite/data_table/charts.py
@@ -0,0 +1,321 @@
+"""
+Pavel Krobot <Pavel.Krobot@cesnet.cz>
+
+Copyright: (C) 2023 CESNET, z.s.p.o.
+
+Common module for plotting of data stored within a DataTable.
+"""
+
+import itertools
+from dataclasses import dataclass, field
+from typing import List, Union
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+@dataclass
+class PlotLineSpec:
+    """Data class representing specification of single line to plot.
+
+    Parameters
+    ----------
+    column : str
+        Name of a pandas.DataFrame column.
+    label_base : str
+        Base name of line label. Label-base is used instead of "direct"
+        label as this base is extended by line identification when
+        combining same kind of lines with different parameters. (e.g.
+        measured value for 8 and 16 workers would have same
+        base - "measured").
+    line_kwargs : dict
+        Additional arguments for pandas.Series.plot method. "label"
+        argument is not allowed as it is created automatically from
+        label_base.
+    """
+
+    column: str
+    label_base: str = None
+    line_kwargs: dict = field(default_factory=dict)
+
+    def __post_init__(self):
+        if not self.label_base:
+            self.label_base = self.column
+
+        assert "label" not in self.line_kwargs, "Do not use 'label' directly in line kwargs"
+
+
+@dataclass
+class PlotSpec:
+    """Data class representing specification of single chart to plot.
+
+    Parameters
+    ----------
+    lines : list[PlotLineSpec]
+        Specification of lines to plot.
+    title : str, optional
+        Chart title.
+    x_column : str, optional
+        Name of pandas.DataFrame column which should be used for x axis.
+    max_column : PlotLineSpec, optional
+        If used, special "max" line is drawn. "Max" column may exists
+        more than once in plotted data. However, this max column in most
+        cases have same values for all used parametrizations. Using
+        this argument "max" line is drawn only once.
+    parametrized_by : Union[str, List[str]]
+        Name(s) of columns holding values of parameters of group of
+        lines with same meaning.
+    filter_by : dict
+        Dictionary where keys are column names and values are column
+        values to filter. Requested chart is plotted on these filtered
+        data only.
+    xscale : dict, optional
+        Scale for x axis. Log2 by default.
+    xlabel : str, optional
+        Label of x axis.
+    ylabel : str, optional
+        Label of y axis.
+    """
+
+    lines: List[PlotLineSpec]
+    title: str = ""
+    x_column: str = None
+    max_column: PlotLineSpec = None
+    parametrized_by: Union[str, List[str]] = None
+    filter_by: dict = None
+    xscale = dict(value="log", base=2)
+    xlabel: str = None
+    ylabel: str = None
+
+    def __post_init__(self):
+        if isinstance(self.parametrized_by, str):
+            self.parametrized_by = [self.parametrized_by]
+
+
+class DataTableCharts:
+    """Class responsible for plotting of tabular data stored within
+    a DataTable
+
+    Attributes
+    ----------
+    _charts : list[list[PlotSpec]]
+        List of lists (i.e. 2D-array) of plot specifications. Defines
+        charts layout of resulting picture. Every plot specification
+        defines one chart which will be placed in resulting figure in
+        same location as within this list.
+        E.g. [[spec1, spec2], [spec3, spec4]] will result in a figure:
+        +----------------------------------+
+        |                                  |
+        |  +===========+    +===========+  |
+        |  |           |    |           |  |
+        |  |  CHART 1  |    |  CHART 2  |  |
+        |  |           |    |           |  |
+        |  +===========+    +===========+  |
+        |                                  |
+        |  +===========+    +===========+  |
+        |  |           |    |           |  |
+        |  |  CHART 3  |    |  CHART 4  |  |
+        |  |           |    |           |  |
+        |  +===========+    +===========+  |
+        |                                  |
+        +----------------------------------+
+        Note: Count of charts in all rows has to be same.
+
+    _data : data_table.DataTable
+        Source data for plotting.
+    """
+
+    def __init__(self, charts=None):
+        self._charts = charts if charts is not None else []
+        self._data = None
+
+        self._n_rows = None
+        self._n_cols = None
+
+    def set_data(self, data):
+        """Set source data"""
+
+        self._data = data
+
+    def append_charts_row(self, charts):
+        """Append single charts row to the list of all charts to plot.
+
+        Parameters
+        ----------
+        charts : list
+            List of charts specification (use list even if single chart
+            in a row is used).
+        """
+
+        self._charts.append(charts)
+
+    def _set_dimensions(self):
+        self._n_rows = len(self._charts)
+        self._n_cols = len(self._charts[0])
+
+    def _assert_layout_grid(self):
+        """Check whether charts layout has same number of columns in all
+        rows.
+        """
+
+        all_n_cols = [len(x) for x in self._charts]
+        c = all_n_cols.count(self._n_cols)
+
+        assert c == self._n_rows, "Layout with various count of charts in rows is not supported."
+
+    def _prepare_layout(self):
+        """Prepare figure and axes using matplotlib."""
+
+        self._set_dimensions()
+
+        self._assert_layout_grid()
+
+        fig_width = self._n_cols * 600 / 100
+        fig_height = self._n_rows * 500 / 100
+        fig, axes = plt.subplots(
+            nrows=self._n_rows,
+            ncols=self._n_cols,
+            figsize=(fig_width, fig_height),
+            dpi=300,
+        )
+
+        fig.tight_layout(pad=6)
+
+        return (fig, axes)
+
+    @staticmethod
+    def _format_ax(ax, spec, data):
+        """Common formatting"""
+
+        ax.set_title(spec.title)
+        ax.legend(fontsize=6)
+        ax.set_xscale(**spec.xscale)
+        ax.set_xticks(data[spec.x_column])
+        ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+        if spec.xlabel:
+            ax.set_xlabel(spec.xlabel)
+        if spec.ylabel:
+            ax.set_ylabel(spec.ylabel)
+        ax.grid(linestyle=":", color="silver")
+
+    def _subframes_by_params(self, data, params):
+        """Filter sub-frames per unique values of selected parameters.
+        From these sub-frames requested columns will be plotted.
+        """
+
+        filters = []
+        for par in params:
+            flt = []
+            for val in data.get_column_unique_values(par):
+                flt.append((par, val))
+            filters.append(flt)
+
+        sub_frames = []
+        for composed_flt in itertools.product(*filters):
+            sub_frames.append(data.filter_data(dict(composed_flt)))
+
+        return sub_frames
+
+    @staticmethod
+    def _label_suffix(parametrized_by, data):
+        """Create line label suffix.
+
+        Suffix is composed from names of all parametrized_by and their current
+        values.
+
+        Note: Only first letter of group name is used to keep labels
+        short.
+        """
+        if not parametrized_by:
+            return ""
+
+        par_sep = ";"
+        suffix = " "
+        for par in parametrized_by:
+            val = data[par].iloc[0]
+            par_name = par[0]
+            suffix = f"{suffix}{par_name}={val}{par_sep}"
+        return suffix[: -len(par_sep)]  # remove trailing separator
+
+    @staticmethod
+    def _plot_line(ax, data, line_spec, index_column, parametrized_by=None):
+        label_suffix = DataTableCharts._label_suffix(parametrized_by, data)
+
+        col = data[line_spec.column]
+        col.index = data[index_column]
+
+        col.plot(
+            kind="line",
+            ax=ax,
+            label=f"{line_spec.label_base}{label_suffix}",
+            **line_spec.line_kwargs,
+        )
+
+    def _chart_spec_and_ax(self, axes, row, column):
+        if axes.ndim == 1:
+            ax = axes[column]
+        else:
+            ax = axes[row, column]
+
+        return self._charts[row][column], ax
+
+    def _lines_subframes(self, ch_spec):
+        if ch_spec.filter_by:
+            data = self._data.filter_data(ch_spec.filter_by)
+        else:
+            data = self._data
+
+        if ch_spec.parametrized_by:
+            return self._subframes_by_params(data, ch_spec.parametrized_by)
+        else:
+            return [data]
+
+    def _plot_data_lines(self, sub_frames, ch_spec, ax):
+        for data in sub_frames:
+            for line_spec in ch_spec.lines:
+                self._plot_line(
+                    ax,
+                    data.get_data(),
+                    line_spec,
+                    ch_spec.x_column,
+                    ch_spec.parametrized_by,
+                )
+
+    def store_charts(self, chart_file, title=None):
+        """Plot requested data into a file.
+
+        Parameters:
+        -----------
+        chart_file : str or Path
+            Path to a resulting file.
+        title : str, optional
+            Title of the figure.
+        """
+
+        fig, axes = self._prepare_layout()
+
+        for i in range(self._n_rows):
+            for j in range(self._n_cols):
+                ch_spec, ax = self._chart_spec_and_ax(axes, i, j)
+
+                sub_frames = self._lines_subframes(ch_spec)
+
+                # All subframes are expected to have same max and index
+                any_subframe = sub_frames[0]
+
+                if ch_spec.max_column:
+                    # Draw "max" line only once
+                    self._plot_line(
+                        ax,
+                        any_subframe.get_data(),
+                        ch_spec.max_column,
+                        ch_spec.x_column,
+                    )
+
+                self._plot_data_lines(sub_frames, ch_spec, ax)
+
+                self._format_ax(ax, ch_spec, any_subframe.get_data())
+
+        if title:
+            fig.suptitle(title, fontsize=18)
+        fig.savefig(chart_file)
---
+++ lbr_testsuite/data_table/data_table.py
@@ -0,0 +1,144 @@
+"""
+Pavel Krobot <Pavel.Krobot@cesnet.cz>
+
+Copyright: (C) 2023 CESNET, z.s.p.o.
+
+Common module for manipulating tabular data.
+"""
+
+from __future__ import annotations  # postponed evaluation of annotations
+
+import pathlib
+from typing import Optional, Union
+
+import pandas
+
+
+class DataTable:
+    """Class responsible for storing tabular data based on
+    pandas.DataFrame.
+    """
+
+    def __init__(self, init_data: Union[tuple, pandas.DataFrame]):
+        """Initialize a data table instance.
+
+        Parameters
+        ----------
+        init_data : Union[tuple, padans.DataFrame]
+            Column names or DataFrame with initialization data.
+        """
+
+        if isinstance(init_data, pandas.DataFrame):
+            self._df = init_data.copy()
+        else:
+            self._df = pandas.DataFrame(columns=init_data)
+
+    def get_data(self) -> pandas.DataFrame:
+        """Return stored data.
+
+        Returns
+        -------
+        pandas.DataFrame
+            Data stored within the DataTable.
+        """
+
+        return self._df
+
+    def empty(self) -> bool:
+        """Check whether a table contains some data
+
+        Returns
+        -------
+        bool
+            False when data table contains some data, True otherwise.
+        """
+
+        return self._df.empty
+
+    def get_column_unique_values(self, column: str) -> list:
+        """Get all unique values from single column.
+
+        Parameters
+        ----------
+        column : str
+            Name of selected column.
+
+        Returns
+        -------
+        list
+            Unique values returned from selected column.
+        """
+
+        return list(self._df[column].unique())
+
+    def _column_is_numeric(self, key: str) -> bool:
+        return self._df[key].dtype.kind in "iufc"
+
+    def _quote_non_numeric(self, key, val):
+        if self._column_is_numeric(key):
+            return val
+        else:
+            return f'"{val}"'
+
+    def _filter_to_query(self, filter_dict: dict) -> str:
+        sub_q = []
+        for key, val in filter_dict.items():
+            val = self._quote_non_numeric(key, val)
+            sub_q.append(f"{key}=={val}")
+
+        return " & ".join(sub_q)
+
+    def _filter_data_frame(self, filter_by: dict) -> pandas.DataFrame:
+        filter_query = self._filter_to_query(filter_by)
+        return self._df.query(filter_query)
+
+    def filter_data(self, filter_by: dict) -> DataTable:
+        """Get data filtered by values of selected columns.
+
+        Parameters:
+        -----------
+        filter_by : dict
+            Dictionary where keys are column names and values are values
+            to filter. Currently only we can filter only by a single
+            value of a column.
+
+        Returns:
+        --------
+        DataTable
+            DataTable containing only rows matching to the filter.
+        """
+
+        return DataTable(self._filter_data_frame(filter_by))
+
+    def append_row(self, row: tuple):
+        """Append one row to the table.
+
+        Note: Values of all columns has to be set.
+
+        Parameters
+        ----------
+        row : tuple
+            Tuple of values to write..
+        """
+
+        current_row = len(self._df.index)
+        self._df.loc[current_row] = row
+
+    def store_csv(self, csv_file: Union[str, pathlib.Path], filter_by: Optional[dict] = None):
+        """Store data from data table to CSV file.
+
+        Parameters
+        ----------
+        csv_file : str or Path
+            Path to an output CSV file.
+        filter_by : dict, optional
+            Dictionary where keys are column names and values are values
+            to filter. Used to filter rows which should be
+            stored. None by default (i.e. save all data).
+        """
+
+        data = self._df
+        if filter_by:
+            data = self._filter_data_frame(filter_by)
+
+        data.to_csv(csv_file, index=False)
