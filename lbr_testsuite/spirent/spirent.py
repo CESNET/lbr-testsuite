CESNET/lbr-testsuite

commit 71ae158d7ee60b4e031eb4df3fe15e1a86836220

spirent: extract method deactivate_all_stream_blocks()

lbr_testsuite/spirent/spirent.py
@@ -239,6 +239,12 @@ class Spirent(Generator):
 
         return stream_blocks_handler
 
+    def deactivate_all_stream_blocks(self):
+        """Deactivate all stream blocks in current configuration."""
+
+        all_stream_blocks = self._stc_handler.stc_stream_block()
+        self._stc_handler.stc_attribute(all_stream_blocks, "Active", "FALSE")
+
     def activate_stream_blocks(self, stream_block_names):
         """Activate stream blocks by names.
 
@@ -255,8 +261,7 @@ class Spirent(Generator):
         """
 
         # deactivate all stream blocks
-        all_stream_blocks = self._stc_handler.stc_stream_block()
-        self._stc_handler.stc_attribute(all_stream_blocks, "Active", "FALSE")
+        self.deactivate_stream_blocks()
 
         # activate requested stream blocks
         stream_blocks = self._stream_blocks_handler(stream_block_names)

commit 14c9ec213a1841ca281860023e6706b24085f506

spirent: fix method name deactivate_stream_block -> deactivate_all_stream_blocks()
Fixes: 71ae158 ("spirent: extract method deactivate_all_stream_blocks()")

lbr_testsuite/spirent/spirent.py
@@ -261,7 +261,7 @@ class Spirent(Generator):
         """
 
         # deactivate all stream blocks
-        self.deactivate_stream_blocks()
+        self.deactivate_all_stream_blocks()
 
         # activate requested stream blocks
         stream_blocks = self._stream_blocks_handler(stream_block_names)