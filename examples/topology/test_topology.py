48e361d4d49d892cd456f653b56bab254222c59f
Dominik Tran <tran@cesnet.cz>
2024-05-21 12:26:58 +0200

    examples: topology: support printing multi-device

--- examples/topology/test_topology.py
+++ examples/topology/test_topology.py
@@ -9,18 +9,29 @@ arguments: `--wired-loopback=<ifc>,<pci-address>`, `--vdevs` and
 these topology arguments see tests help.
 """
 
+from lbr_testsuite.topology.device import MultiDevice
 from lbr_testsuite.topology.topology import select_topologies
 
 
 select_topologies(["wired_loopback", "vdev_loopback", "wired_spirent", "example_topology"])
 
 
-def test_topology(device, generator, analyzer):
-    print()
-    print("Device:")
+def _print_device(device):
     print(f"    type: {type(device)}")
     print(f"    name: {device.get_dpdk_name()}")
     print(f"    args: {device.get_dpdk_args()}")
+
+
+def test_topology(device, generator, analyzer):
+    print()
+    print("Device:")
+
+    if isinstance(device, MultiDevice):
+        for dev in device.get_base_devices():
+            _print_device(dev)
+    else:
+        _print_device(device)
+
     print("Generator:")
     print(f"    type: {type(generator)}")
     print("Analyzer:")
