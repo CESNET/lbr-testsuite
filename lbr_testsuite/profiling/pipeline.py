CESNET/lbr-testsuite

commit 6e90b2830058417dfe1c64f974c08593bfe35cca

profiling: introduce extracted package
The extracted package provides tools for profiling executables.

The profiling package was extracted 1:1 from the DDoS Protector's 'ddp'
repository.

lbr_testsuite/profiling/pipeline.py
@@ -1,0 +1,239 @@
+"""
+Author(s): Jan Viktorin <viktorin@cesnet.cz>
+
+Copyright: (C) 2023 CESNET, z.s.p.o.
+
+Implementation of profiler reading various Pipeline-specific stats.
+"""
+
+import time
+
+import matplotlib.pyplot as plt
+import pandas
+
+from .profiler import ProfiledSubject, ProfilerMarker, ThreadedProfiler
+
+
+class ProfiledPipelineSubject(ProfiledSubject):
+    def __init__(self, pipeline):
+        super().__init__(pipeline.get_pid())
+        self._pipeline = pipeline
+
+    def get_pipeline(self):
+        return self._pipeline
+
+    def __repr__(self):
+        sys_if = self._pipeline.get_sys_if()
+        return f"subject-{self.get_pid()}-{sys_if}"
+
+
+class PipelineMonProfiler(ThreadedProfiler):
+    def __init__(self, csv_file, mark_file, png_file_pattern, time_step=0.1):
+        super().__init__()
+
+        self._csv_file = csv_file
+        self._mark_file = mark_file
+        self._png_file_pattern = png_file_pattern
+        self._time_step = time_step
+
+    def start(self, subject: ProfiledSubject):
+        if not isinstance(subject, ProfiledPipelineSubject):
+            raise RuntimeError("subject must be of type ProfiledPipelineSubject")
+
+        self._marker = ProfilerMarker()
+        super().start(subject)
+
+    def _make_timestamps_relative(self, timestamps, lowest=None):
+        if lowest is None:
+            lowest = timestamps.min()
+
+        return timestamps.sub(lowest).add(1).round(2).astype("float")
+
+    def _plot_to_file(self, plot, png_file, legend=True):
+        if legend:
+            plot.legend(fontsize=8, bbox_to_anchor=(1, 1), ncol=2)
+        f = plot.get_figure()
+        f.set_layout_engine("tight")
+        self._logger.info(f"save PNG file: {png_file}")
+        plot.get_figure().savefig(png_file)
+
+    def _plot_latencies(self, df, ax, label="latencies", column="latency"):
+        return df.plot(
+            title=f"Pipeline {label}",
+            xlabel="time [s]",
+            ylabel="latency [us]",
+            kind="line",
+            style=".-",
+            x="timestamp",
+            y=filter(lambda k: k.startswith(f"{column}_"), df.columns),
+            figsize=(len(df["timestamp"]) * 0.2, 30),
+            legend=False,
+            ax=ax,
+        )
+
+    def _draw_marks(self, axes, time_lowest):
+        for mark_time in self._marker:
+            time = float(round(mark_time - time_lowest + 1, 2))
+
+            self._logger.debug(f"mark at {time}s")
+
+            for ax in axes:
+                ax.axvline(
+                    x=time,
+                    color="b",
+                    gapcolor="r",
+                    linestyle="--",
+                    alpha=0.3,
+                    label=f"mark: {time}s",
+                )
+
+    def _plot_general(self, df):
+        df = df.copy()
+        lowest = df["timestamp"].min()
+        df["timestamp"] = self._make_timestamps_relative(df["timestamp"], lowest=lowest)
+
+        fig, axes = plt.subplots(nrows=5, ncols=1)
+
+        self._plot_latencies(df, axes[0])
+        self._plot_latencies(df, axes[1], label="maximal latencies", column="max_latency")
+
+        self._plot_chain_calls(df, axes[2])
+        self._plot_seen_pkts(df, axes[3])
+        self._plot_drop_pkts(df, axes[4])
+
+        self._draw_marks(axes, lowest)
+
+        png_file = str(self._png_file_pattern).format("general")
+        self._plot_to_file(fig, png_file)
+
+    def _plot_stage_latencies(self, df, proc_names):
+        png_file = str(self._png_file_pattern).format("stage_latencies")
+
+        lowest = df["timestamp"].min()
+        df["timestamp"] = self._make_timestamps_relative(df["timestamp"], lowest=lowest)
+        df = df.filter(
+            items=filter(
+                lambda k: k.startswith("stage_cur_latency") or k == "timestamp",
+                df.columns,
+            )
+        )
+        df = df.rename(columns=lambda name: name.replace("stage_cur_latency_", ""))
+        fig, axes = plt.subplots(nrows=len(proc_names), ncols=1, sharey=True)
+
+        for name, ax in zip(proc_names, axes):
+            df.plot(
+                title=f"Stage {name} latency",
+                xlabel="time [s]",
+                ylabel=f"{name} latency [us]",
+                kind="line",
+                style=".-",
+                x="timestamp",
+                y=filter(lambda k: k.startswith(name), df.columns),
+                figsize=(len(df["timestamp"]) * 0.2, 30),
+                legend=False,
+                ax=ax,
+            )
+
+        self._draw_marks(axes, lowest)
+
+        self._plot_to_file(fig, png_file)
+
+    def _plot_cumulative_data(self, df, ax, label, column):
+        df = df.copy()
+
+        for name in df.columns:
+            if name.startswith(f"{column}_"):
+                df[name] = df[name].diff()
+
+        return df.plot(
+            title=f"Pipeline {label}",
+            xlabel="time [s]",
+            ylabel=f"{label}",
+            kind="line",
+            style=".-",
+            x="timestamp",
+            y=filter(lambda k: k.startswith(f"{column}_"), df.columns),
+            figsize=(len(df["timestamp"]) * 0.2, 30),
+            legend=False,
+            ax=ax,
+        )
+
+    def _plot_chain_calls(self, df, ax):
+        return self._plot_cumulative_data(df, ax, label="chain calls", column="chain_calls")
+
+    def _plot_seen_pkts(self, df, ax):
+        return self._plot_cumulative_data(df, ax, label="seen packets (volume)", column="seen_pkts")
+
+    def _plot_drop_pkts(self, df, ax):
+        return self._plot_cumulative_data(
+            df, ax, label="dropped packets (last stage)", column="drop_pkts"
+        )
+
+    def mark(self):
+        self._marker.mark(time.monotonic())
+
+    def run(self):
+        pipeline = self._subject.get_pipeline()
+        workers = pipeline.get_workers_count()
+        proc_names = pipeline.get_pipeline_stage_names()
+        data = {"timestamp": []}
+
+        for i in range(workers):
+            data[f"max_latency_{i}"] = []
+            data[f"latency_{i}"] = []
+            data[f"chain_calls_{i}"] = []
+            data[f"seen_pkts_{i}"] = []
+            data[f"drop_pkts_{i}"] = []
+            for name in proc_names:
+                data[f"stage_max_latency_{name}_{i}"] = []
+                data[f"stage_cur_latency_{name}_{i}"] = []
+
+        pipeline.wait_until_pipeline_active()
+
+        while not self.wait_stoppable(self._time_step):
+            now = time.monotonic()
+            status = []
+            chain_status = []
+
+            for i in range(workers):
+                status.append(pipeline.get_worker_status(i))
+                chain_status.append(pipeline.get_worker_chain_status(i))
+
+            data["timestamp"].append(now)
+
+            for i, s in enumerate(status):
+                max_latency, unit = s["max_latency"].split(" ", 2)
+                assert unit == "us"
+
+                latency, unit = s["cur_latency"].split(" ", 2)
+                assert unit == "us"
+
+                chain_calls = int(s["chain_calls"])
+                seen_pkts = int(s["seen_pkts"])
+                drop_pkts = int(s["drop_pkts"])
+
+                data[f"max_latency_{i}"].append(float(max_latency))
+                data[f"latency_{i}"].append(float(latency))
+                data[f"chain_calls_{i}"].append(chain_calls)
+                data[f"seen_pkts_{i}"].append(seen_pkts)
+                data[f"drop_pkts_{i}"].append(drop_pkts)
+
+                for j, name in enumerate(proc_names):
+                    stage_max, unit = chain_status[i][f"max_latency[{j}]"].split(" ", 2)
+                    assert unit == "us"
+                    stage_cur, unit = chain_status[i][f"cur_latency[{j}]"].split(" ", 2)
+                    assert unit == "us"
+
+                    data[f"stage_max_latency_{name}_{i}"].append(float(stage_max))
+                    data[f"stage_cur_latency_{name}_{i}"].append(float(stage_cur))
+
+        self._logger.info(f"sampled {len(data)}x pipeline status")
+
+        df = pandas.DataFrame(data)
+        df.to_csv(self._csv_file)
+
+        with open(self._mark_file, "w") as f:
+            self._marker.save(f)
+
+        self._plot_general(df)
+        self._plot_stage_latencies(df, proc_names)

lbr_testsuite/profiling/cache.py
@@ -1,0 +1,371 @@
+"""
+Author(s): Kamil Vojanec <vojanec@cesnet.cz>
+
+Copyright: (C) 2024 CESNET, z.s.p.o.
+
+Implementation of profiler for measuring per-process data.
+"""
+
+import os
+import time
+from collections import defaultdict
+from contextlib import ExitStack, contextmanager
+from pathlib import Path
+from typing import Dict, List, NamedTuple
+
+import matplotlib.pyplot as plt
+import pandas
+from pypapi import papi_low as papi
+from pypapi.exceptions import PapiNoEventError
+
+from .profiler import ProfilerMarker, ThreadedProfiler
+
+
+class ThreadInfo(NamedTuple):
+    """Utility class for storing thread information."""
+
+    id: int
+    name: str
+
+
+def find_dpdk_worker_threads(pid: int) -> List[ThreadInfo]:
+    """Utility function used to lookup threads managed by given pid and
+    filter threads whose names start with `dpdk-worker`.
+
+    Parameters
+    ----------
+    pid : int
+        PID of the process whose threads to look up.
+
+    Returns
+    -------
+    List[ThreadInfo]
+        List of thread informations corresponding to individual
+        DPDK worker threads.
+    """
+
+    ret = []
+    for thread_dir in Path(f"/proc/{pid}/task").iterdir():
+        tid = int(thread_dir.name)
+        name = (thread_dir / "comm").read_text().strip()
+
+        if name.startswith("dpdk-worker"):
+            ret.append(ThreadInfo(tid, name))
+
+    return ret
+
+
+class PapiThreadContext:
+    """Class wrapping the PAPI eventset providing methods
+    to start, stop and sample the PAPI counters."""
+
+    def __init__(
+        self,
+        eventset: int,
+        event_list: List[int],
+        thread: ThreadInfo,
+    ):
+        """
+        Parameters
+        ----------
+        eventset : int
+            Integer handle to the underlying PAPI eventset.
+        event_list : List[int]
+            List of events managed by the eventset.
+        thread : ThreadInfo
+            Thread to which the eventset belongs.
+        """
+
+        self._eventset = eventset
+        self._event_list = event_list
+        self._thread = thread
+
+    def start(self):
+        """Start measuring PAPI counters of underlying eventset."""
+
+        papi.start(self._eventset)
+
+    def sample(self) -> Dict[str, int]:
+        """Sample counters of the underlying eventset.
+
+        Returns
+        -------
+        Dict[str, int]
+            Dictionary with thread names and counter names as keys
+            and the counter values as values.
+        """
+
+        zero_samples = [0 for _ in self._event_list]
+        samples = papi.accum(self._eventset, zero_samples)
+
+        return {
+            f"{self._thread.name}_{papi.event_code_to_name(cnt)}": value
+            for cnt, value in zip(self._event_list, samples)
+        }
+
+    def stop(self):
+        """Stop measuring PAPI counters of underlying eventset."""
+
+        papi.stop(self._eventset)
+
+
+class PapiMultiThreadContext:
+    """Wrapper over multiple PapiThreadContext for measuring
+    more threads at once."""
+
+    def __init__(self, contexts: List[PapiThreadContext]):
+        """
+        Parameters
+        ----------
+        contexts : List[PapiThreadContext]
+            Managed list of PapiThreadContext objects.
+        """
+
+        self._contexts = contexts
+
+    def start(self):
+        """Start measurement of all underlying contexts."""
+
+        for c in self._contexts:
+            c.start()
+
+    def sample(self) -> Dict[str, int]:
+        """Sample the PAPI counters of all underlying contexts and
+        compose them into a single dictionary.
+
+        Returns
+        -------
+        Dict[str, int]
+            Dictionary with thread names and counter names as keys
+            and the counter values as values.
+        """
+
+        ret = {}
+        for c in self._contexts:
+            s = c.sample()
+            ret = {**ret, **s}
+
+        return ret
+
+    def stop(self):
+        """Stop the measurement of all underlying contexts."""
+
+        for c in self._contexts:
+            c.stop()
+
+
+@contextmanager
+def papi_context_manager(event_list: List[int], thread: ThreadInfo):
+    """Context manager used to manage an instance of PapiThreadContext
+    for a given thread.
+
+    Parameters
+    ----------
+    event_list : List[int]
+        List of events to be measured.
+    thread : ThreadInfo
+        Object representing the thread which is measured.
+
+    Returns
+    -------
+    PapiThreadContext
+        Managed instance of context for given thread.
+    """
+
+    eventset = papi.create_eventset()
+    papi.assign_eventset_component(eventset, 0)  # << By convention, 0 is always the cpu component.
+
+    for ev in event_list:
+        papi.add_event(eventset, ev)
+
+    papi.attach(eventset, thread.id)
+
+    yield PapiThreadContext(eventset, event_list, thread)
+
+    papi.detach(eventset)
+    papi.cleanup_eventset(eventset)
+    papi.destroy_eventset(eventset)
+
+
+@contextmanager
+def papi_multi_context_manager(event_list: List[int], threads: List[ThreadInfo]):
+    """Context manager managing multiple contexts within a single PapiMultiThreadContext.
+
+    Parameters
+    ----------
+    event_list : List[int]
+        List of PAPI events to be measured.
+    threads : List[ThreadInfo]
+        List of threads to be measured.
+
+    Returns
+    -------
+    PapiMultiThreadContext
+        Wrapper object over multiple PapiThreadContext for multi threaded measurement.
+    """
+
+    # ExitStack object can be used to manage multiple context manager as one.
+    # By calling 'enter_context()' the provided context manager is entered and stored
+    # within the ExitStack object. At the end of the 'with' block, the stack object
+    # is destroyed together with all underlying context managers.
+    # See https://docs.python.org/3/library/contextlib.html#contextlib.ExitStack
+    with ExitStack() as stack:
+        contexts = [stack.enter_context(papi_context_manager(event_list, t)) for t in threads]
+        yield PapiMultiThreadContext(contexts)
+
+
+class PAPIProfiler(ThreadedProfiler):
+    """Profiler for cache events using PAPI events."""
+
+    def __init__(
+        self,
+        csv_file: Path,
+        mark_file: str,
+        png_file: str,
+        event_groups: Dict[str, List],
+        time_step: float = 0.1,
+    ):
+        """
+        Parameters
+        ----------
+        csv_file : Path
+            Path to the output csv file.
+        png_file_pattern : str
+            Base of the png file name.
+        event_groups : Dict[str, List]
+            Event groups dictionary in the format:
+                {"name": list of events in group}
+        time_step : float
+            Time step between two consecutive measurements.
+        """
+
+        super().__init__()
+
+        self._csv_file = csv_file
+        self._mark_file = mark_file
+        self._png_file = png_file
+        self._time_step = time_step
+
+        papi_ver = int(os.environ.get("PYPAPI_VER", 0x7010000))
+
+        if papi.is_initialized() == 0:
+            papi.library_init(papi_ver)
+
+        self._event_groups = event_groups
+        event_list = list({ev for evlist in event_groups.values() for ev in evlist})
+        self._event_list = self._filter_supported_events(event_list)
+
+    def _filter_supported_events(self, event_list: List[int]) -> List[int]:
+        ret = []
+        for ev in event_list:
+            try:
+                papi.query_event(ev)
+            except PapiNoEventError:
+                self._logger.warning(f"Event {papi.event_code_to_name(ev)} not found, skipping...")
+                continue
+
+            ret.append(ev)
+
+        return ret
+
+    @staticmethod
+    def _make_timestamps_relative(timestamps, lowest=None):
+        if lowest is None:
+            lowest = timestamps.min()
+
+        return timestamps.sub(lowest).add(1).round(2).astype("float")
+
+    def _filter_subframe(self, df: pandas.DataFrame, events: List[int]) -> pandas.DataFrame:
+        def is_event(val: str, events: List[int]) -> bool:
+            for ev in events:
+                if papi.event_code_to_name(ev) in val:
+                    return True
+            else:
+                return False
+
+        return df.filter(
+            items=filter(
+                lambda k: is_event(k, events) or k == "timestamp",
+                df.columns,
+            )
+        )
+
+    def _draw_marks(self, axes, time_lowest):
+        for mark_time in self._marker:
+            time = float(round(mark_time - time_lowest + 1, 2))
+
+            self._logger.debug(f"mark at {time}s")
+
+            for ax in axes:
+                ax.axvline(
+                    x=time,
+                    color="b",
+                    gapcolor="r",
+                    linestyle="--",
+                    alpha=0.3,
+                    label=f"mark: {time}s",
+                )
+
+    def _plot_events_cumulative(
+        self,
+        df: pandas.DataFrame,
+    ):
+        lowest = df["timestamp"].min()
+        df["timestamp"] = self._make_timestamps_relative(df["timestamp"], lowest=lowest)
+
+        fig, axes = plt.subplots(nrows=len(self._event_groups), ncols=1, sharex=True)
+
+        for group, ax in zip(self._event_groups.keys(), axes):
+            df_local = self._filter_subframe(df, self._event_groups[group])
+            df_local.plot(
+                title=f"PAPI events: {group}",
+                xlabel="time [s]",
+                ylabel="event count",
+                kind="line",
+                style=".-",
+                x="timestamp",
+                figsize=(len(df["timestamp"]) * 0.2, 15),
+                legend=True,
+                ax=ax,
+            )
+
+        self._draw_marks(axes, lowest)
+
+        fig.set_layout_engine("tight")
+        self._logger.info(f"saving PNG file: {self._png_file}")
+        fig.savefig(self._png_file)
+
+    def start(self, subject):
+        self._marker = ProfilerMarker()
+        super().start(subject)
+
+    def mark(self):
+        self._marker.mark(time.monotonic())
+
+    def run(self):
+        pid = self._subject.get_pid()
+        workers = find_dpdk_worker_threads(pid)
+        data = defaultdict(list)
+
+        with papi_multi_context_manager(self._event_list, workers) as c:
+            c.start()
+
+            while not self.wait_stoppable(self._time_step):
+                now = time.monotonic()
+                data["timestamp"].append(now)
+
+                samples = c.sample()
+                for s_name, s_value in samples.items():
+                    data[s_name].append(s_value)
+
+            c.stop()
+
+        self._logger.info(f"sampled {len(data)}x cache status")
+
+        df = pandas.DataFrame(data)
+        df.to_csv(self._csv_file)
+
+        with open(self._mark_file, "w") as f:
+            self._marker.save(f)
+
+        self._plot_events_cumulative(df)

lbr_testsuite/profiling/cpumon.py
@@ -1,0 +1,179 @@
+"""
+Author(s): Jan Viktorin <viktorin@cesnet.cz>
+
+Copyright: (C) 2023-2024 CESNET, z.s.p.o.
+
+Implementation of profiler measuring CPU frequencies.
+"""
+
+import logging
+import time
+from collections import defaultdict
+
+import pandas
+import psutil
+
+from .profiler import ThreadedProfiler
+
+
+global_logger = logging.getLogger(__name__)
+
+
+class CPUMonProfiler(ThreadedProfiler):
+    """Profiler that is running a thread that continuously collects data
+    about CPU frequencies. It plots two charts:
+
+    * count of CPUs in time at certain frequency levels
+    * relative time spent at certain frequency level for each CPU during measurement
+
+    Frequency measurements are stored in CSV file named _raw_ in their original form.
+    The particular outputs then consist of a CSV file and PNG file (with chart), here
+    the frequencies are rounded to hundred MHz (the original precision is not useful).
+    """
+
+    def __init__(self, csv_file_pattern, png_file_pattern, time_step=0.1):
+        super().__init__()
+
+        self._csv_file_pattern = csv_file_pattern
+        self._png_file_pattern = png_file_pattern
+        self._time_step = time_step
+
+    def _cpu_names(self):
+        names = []
+
+        for i in range(len(psutil.cpu_freq(True))):
+            names.append(f"cpu_{i}")
+
+        return names
+
+    def _make_timestamps_relative(self, timestamps):
+        return timestamps.sub(timestamps.min()).add(1).round(2).astype("float")
+
+    def _round_freq(self, freq):
+        return round(freq / 100) * 100
+
+    def _collect_cpu_freq_columns(self, df):
+        return list(filter(lambda name: name.startswith("cpu_"), df.columns))
+
+    def _collect_freqs(self, df, cpus):
+        all_freqs = set()
+
+        for i in range(len(df)):
+            for cpu in cpus:
+                all_freqs.add(self._round_freq(df.iloc[i].loc[cpu]))
+
+        return all_freqs
+
+    def _data_to_df(self, data, csv_file):
+        df = pandas.DataFrame(data)
+        global_logger.info(f"save CSV file: {csv_file}")
+        df.to_csv(csv_file)
+
+        return df
+
+    def _plot_to_png(self, p, png_file, legend_cols=2):
+        p.legend(fontsize=8, bbox_to_anchor=(1, 1), ncol=legend_cols)
+        f = p.get_figure()
+        f.set_layout_engine("tight")
+        global_logger.info(f"save PNG file: {png_file}")
+        p.get_figure().savefig(png_file)
+
+    def _plot_freqs_per_cpu(self, df, csv_file, png_file):
+        cpus = self._collect_cpu_freq_columns(df)
+        all_freqs = self._collect_freqs(df, cpus)
+        data = defaultdict(list)
+
+        for cpu in cpus:
+            freq_hist = {}
+
+            for freq in all_freqs:
+                freq_hist[freq] = 0
+
+            for i in range(len(df)):
+                freq_hist[self._round_freq(df.iloc[i].loc[cpu])] += 1
+
+            data["cpu"].append(cpu)
+
+            freq_total = sum(freq_hist.values())
+            for freq, count in freq_hist.items():
+                data[freq].append(100 * count / freq_total)
+
+        df = self._data_to_df(data, csv_file)
+
+        p = df.plot(
+            title="CPU frequencies",
+            xlabel="CPU",
+            ylabel="Time at frequency level (%)",
+            kind="bar",
+            x="cpu",
+            stacked=True,
+            legend=True,
+            figsize=(int(len(cpus) * 0.3), 8),
+        )
+
+        self._plot_to_png(p, png_file)
+
+    def _make_freqs_hist(self, df, cpus, row):
+        hist = {}
+
+        for freq in self._collect_freqs(df, cpus):
+            hist[freq] = 0
+
+        for cpu in cpus:
+            hist[self._round_freq(df.iloc[row].loc[cpu])] += 1
+
+        return hist
+
+    def _plot_freqs(self, df, csv_file, png_file):
+        cpus = self._collect_cpu_freq_columns(df)
+        data = defaultdict(list)
+
+        for i in range(len(df)):
+            data["timestamp"].append(df.iloc[i].loc["timestamp"])
+
+            for freq, count in self._make_freqs_hist(df, cpus, i).items():
+                data[freq].append(count)
+
+        df = self._data_to_df(data, csv_file)
+
+        p = df.plot(
+            title="CPU frequencies in time (MHz)",
+            xlabel="time [s]",
+            ylabel=f"CPU count (out of {len(cpus)} CPUs)",
+            kind="line",
+            x="timestamp",
+            yticks=list(range(0, len(cpus) + 1, 3)),
+            legend=True,
+        )
+
+        self._plot_to_png(p, png_file)
+
+    def run(self):
+        cpu_names = self._cpu_names()
+        data = {"timestamp": []}
+
+        for name in cpu_names:
+            data[name] = []
+
+        while not self.wait_stoppable(self._time_step):
+            now = time.monotonic()
+            data["timestamp"].append(now)
+
+            for i, freq in enumerate(psutil.cpu_freq(True)):
+                data[f"cpu_{i}"].append(freq.current)
+
+        df = pandas.DataFrame(data)
+        df.to_csv(str(self._csv_file_pattern).format("raw"))
+
+        df["timestamp"] = self._make_timestamps_relative(df["timestamp"])
+
+        self._plot_freqs_per_cpu(
+            df,
+            str(self._csv_file_pattern).format("freqs_per_cpu"),
+            str(self._png_file_pattern).format("freqs_per_cpu"),
+        )
+        self._plot_freqs(
+            df,
+            str(self._csv_file_pattern).format("freqs"),
+            str(self._png_file_pattern).format("freqs"),
+        )

lbr_testsuite/profiling/perf.py
@@ -1,0 +1,72 @@
+"""
+Author(s): Jan Viktorin <viktorin@cesnet.cz>
+
+Copyright: (C) 2021-2024 CESNET, z.s.p.o.
+
+Implementation of perf profiler.
+"""
+
+from lbr_testsuite import executable
+
+from .profiler import PidProfiler
+
+
+class PerfDaemon(executable.Daemon):
+    def _handle_failure(self, process_cmd, process_retcode, stdout, stderr):
+        """Avoid false-positive reporting. The perf record programs returns
+        -2 or -15 when properly terminated.
+        """
+
+        if process_retcode != -2 and process_retcode != -15:
+            super()._handle_failure(process_cmd, process_retcode, stdout, stderr)
+        return stdout, stderr
+
+
+class AbstractPerf(PidProfiler):
+    def __init__(self, data, args=[], logger=None, env=None):
+        super().__init__(cmd=self.DEFAULT_CMD, logger=logger, env=env)
+        self._data = str(data)
+        self._args = args
+
+    def _create_daemon(self, cmd):
+        return PerfDaemon(command=cmd, logger=self._logger, env=self._env)
+
+
+class Perf(AbstractPerf):
+    DEFAULT_CMD = ("perf", "record")
+
+    def _build_cmd(self, pid):
+        cmd = self._copy_cmd()
+        cmd.extend(["-o", self._data, "-p", str(pid), "--all-user"])
+        cmd.extend(self._args)
+        return cmd
+
+
+class PerfStat(AbstractPerf):
+    DEFAULT_CMD = ("perf", "stat", "record")
+
+    def _build_cmd(self, pid):
+        cmd = self._copy_cmd()
+        cmd.extend(["-o", self._data, "-p", str(pid)])
+        cmd.extend(self._args)
+        return cmd
+
+
+class PerfMem(AbstractPerf):
+    DEFAULT_CMD = ("perf", "mem", "record")
+
+    def _build_cmd(self, pid):
+        cmd = self._copy_cmd()
+        cmd.extend(["--", "-o", self._data, "-p", str(pid), "--all-user"])
+        cmd.extend(self._args)
+        return cmd
+
+
+class PerfC2C(AbstractPerf):
+    DEFAULT_CMD = ("perf", "c2c", "record")
+
+    def _build_cmd(self, pid):
+        cmd = self._copy_cmd()
+        cmd.extend(["--", "-o", self._data, "-p", str(pid), "--all-user"])
+        cmd.extend(self._args)
+        return cmd

commit 2ecbaeb06a7bc39f14c17e356b5fd225bafdf604

profiling: fix exception when generating single subplot
This commit fixes the following error:

```
14:36:27 ERROR PipelineMonProfiler: profiler subject-50159-<unknown> has failed
Traceback (most recent call last):
  File "/.../pytest/.venv/lib/python3.11/site-packages/lbr_testsuite/profiling/profiler.py", line 171, in run_safe
    self.run()
  File "/.../pytest/.venv/lib/python3.11/site-packages/lbr_testsuite/profiling/pipeline.py", line 307, in run
    self._plot_stage_latencies(df, context.get_stages())
  File "/.../pytest/.venv/lib/python3.11/site-packages/lbr_testsuite/profiling/pipeline.py", line 236, in _plot_stage_latencies
    for name, ax in zip(proc_names, axes):
                    ^^^^^^^^^^^^^^^^^^^^^
TypeError: 'Axes' object is not iterable
```

The error was caused by attempting to generate subplots with only one
row and one column (i.e. a single plot). In that case,
matplotlib.subplots() method only outputs a single Axes object instead
of a list of Axes objects.

This commit fixes the issue by wrapping the object into a list first.

Fixes: 6e90b283 ("profiling: introduce extracted package")

lbr_testsuite/profiling/pipeline.py
@@ -233,6 +233,12 @@ class PipelineMonProfiler(ThreadedProfiler):
         df = df.rename(columns=lambda name: name.replace("stage_cur_latency_", ""))
         fig, axes = plt.subplots(nrows=len(proc_names), ncols=1, sharey=True)
 
+        # When matplotlib.subplots() generates only a single plot, the output object
+        # is just Axes (instead of a list of Axes), which is not iterable. We need to
+        # convert it to a list in order to allow iteration.
+        if len(proc_names) == 1:
+            axes = [axes]
+
         for name, ax in zip(proc_names, axes):
             df.plot(
                 title=f"Stage {name} latency",